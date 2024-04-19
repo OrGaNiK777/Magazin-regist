26fa2bff297eeb8950902ee79ceffa48a3cf3c4a
OrGaNiK777 <OrGaNiK777@yandex.ru>
2024-04-19 22:30:37 +0500

    dom v6

--- src/init/cards.js
+++ src/init/cards.js
@@ -6,14 +6,16 @@ export const initialCards = [
     link3: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link4: "https://avatars.dzeninfra.ru/get-zen_doc/10315851/pub_64aa8b585a90544259201280_64aa8b823bc2b94ba36b46dc/scale_1200",
     link5: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    price: 100
+    price: 100,
+    id: "wdqw332232e233e2"
   },
   {
     name: "Архыз2",
     link1: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link2: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link3: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    link4: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg"
+    link4: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    id: "wdqw3322sd32ef233e2"
   },
   {
     name: "Архыз3",
@@ -21,53 +23,60 @@ export const initialCards = [
     link2: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link3: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link4: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    link5: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg"
+    link5: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    id: "wdqw3322sd32e233ed2asdas"
   }, {
     name: "Архыз4",
     link1: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link2: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link3: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link4: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    link5: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg"
+    link5: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    id: "wdqw3322sd3ss2e2ww33e2asdas"
   }, {
     name: "Архыз5",
     link1: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link2: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link3: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link4: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    link5: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg"
+    link5: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    id: "wdqw3322sd32e233qqe2asdas"
   },
   {
-    name: "Архыз5",
+    name: "Архыз6",
     link1: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link2: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link3: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link4: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    link5: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg"
+    link5: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    id: "wdqw3322sd32e233de2asdas"
   },
   {
-    name: "Архыз5",
+    name: "Архыз7",
     link1: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link2: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link3: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link4: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    link5: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg"
+    link5: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    id: "wdqw3a322sd32e233e2asdas"
   },
   {
-    name: "Архыз5",
+    name: "Архыз8",
     link1: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link2: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link3: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link4: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    link5: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg"
+    link5: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    id: "wdqw3322ssd32e233e2asdas"
   },
   {
-    name: "Архыз5",
+    name: "Архыз9",
     link1: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link2: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link3: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     link4: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    link5: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg"
+    link5: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    id: "wdqw3322sdw32e233e2asdas"
   },
   
 ];
--- src/js/main.js
+++ src/js/main.js
@@ -4,29 +4,22 @@ function createCards(item) {
   const cardTemplate = document.getElementById("card-template").content.querySelector(".col").cloneNode(true);
   cardTemplate.querySelectorAll(".name").forEach((el) => { el.textContent = item.name; })
   cardTemplate.querySelectorAll(".link1").forEach((el) => { el.src = item.link1 })
+  cardTemplate.querySelector('#staticBackdrop').id = item.id
+  cardTemplate.querySelector("#buttonModal").href = "about:blank#" + item.id
 
-  const modal = cardTemplate.querySelector('#staticBackdrop')
-  console.log(initialCards)
-  //console.log(modal.id = modal.id + cardTemplate.length)
-  modal.id = modal.id + length
+  if (item.price) { cardTemplate.querySelectorAll(".price").forEach((el) => { el.textContent = "Цена: " + item.price + " руб."; }) }
+  else { cardTemplate.querySelectorAll(".price").forEach((el) => { el.textContent = "Цена: Узнать лично" }) }
 
-  // const button = cardTemplate.querySelector('#buttonModal')
-  // console.log(button)
-  // console.log(button.data - bs - target)
-
-  if (item.price) { cardTemplate.querySelectorAll(".price").forEach((el) => { el.textContent = item.price + " руб."; }) }
-  else { cardTemplate.querySelector(".price").textContent = "Узнать лично"; }
-
-  // cardTemplate.querySelector(".link2").s rc = item.link2;
-  // cardTemplate.querySelector(".link3").src = item.link3;
-  // cardTemplate.querySelector(".link4").src = item.link4;
-  // cardTemplate.querySelector(".link5").src = item.link5;
+  cardTemplate.querySelector(".link2").src = item.link2;
+  cardTemplate.querySelector(".link3").src = item.link3;
+  cardTemplate.querySelector(".link4").src = item.link4;
+  cardTemplate.querySelector(".link5").src = item.link5;
   return cardTemplate;
 }
 
 function rendererCard(items) {
   items.forEach((item) => {
-    document.querySelector(".cards1").append(createCards(item));
+    document.querySelector(".cards").append(createCards(item));
   });
 }
 
